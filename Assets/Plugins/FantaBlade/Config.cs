4c8bdf720aaba7669f6de8dc0b2b4844c0721b33
wangxinji <wangxinji@fantablade.com>
2023-01-30 18:04:12 +0800

    [modify] 集成数数sdk

---
+++ Assets/Plugins/FantaBlade/Config.cs
@@ -0,0 +1,16 @@
+﻿namespace FantaBlade
+{
+    /// <summary>
+    ///     幻刃sdk及三方授权需要的所有参数
+    /// </summary>
+    public static class Config
+    {
+        public const string WECHAT_APPID = "wxeacc4ec2f2d5f24e";
+        public const string WECHAT_UNIVERSAL_LINK = "https://watergun.hotfix.huanrengame.com/fbsdk/";
+        public const string WEIBO_APPID = "1858163759";
+        public const string WEIBO_REDIRECTURL = "https://www.fantablade.com/phantomoon/index";
+        public const string QQ_APPID = "101940541";
+        public const string DOUYIN_CLIENTKEY = "awr89o05lcbk46n2";
+        public const string MOBILE_SECRETINFO = "4e0zZH5OoxmyDn0ccbIyMWzi885zteHieCGOLmZ/thqVMl1kjCP/Fqjk1KW+OEaZ2QFS7IbHRBruzPQ0BaFu8JQJcBhFvHBRKpKmCFFUKy3qGU4g71es2pKjwdn2oI2A1Wkf4vNm/BUrD0ZhOarv+8mvHhVvOofYXD29g0Y9fVZnWX9XvH5lMswEnX+CzjJHIgRh8LbaI6u/qImf2BOC8oA3/0QrTSjZKAnp3TBYTn2ZblN1sQ1cYLiVsnccceUKcapiOQFAks3UlOsjgtYb8pf1SDsu9yEN4jaCIkVzyJAE4mGj/tP6Go3A1vKkY9VQ";
+    }
+}
